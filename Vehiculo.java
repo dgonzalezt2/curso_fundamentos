54967003d9e6788330e37a076d35e727bbd61b32
dgonzalezt2 <81880494+dgonzalezt2@users.noreply.github.com>
2021-04-27 22:27:13 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,28 +1,30 @@
 public class Vehiculo {
-    //atributos de la clase
+    //Atributos
     public static int tamano;
     public static int cantidad = 0;
     private String placa;
     private String marca;
     private String color;
     private int valorComercial;
-
     public static Vehiculo vehiculos [][];
-    public Vehiculo (){}
+    
+    
+    public Vehiculo (){}//Constructor sin parametros
 
-    public Vehiculo(String plac, String marc, String colo) {
-        this(plac, marc, colo, 30000000);
+    public Vehiculo(String plac, String marc, String colo) {//Constructor con 3 parámetros
+        this(plac, marc, colo, 30000000);                   //Asigna por defecto un valor de 30000000
     }
 
-    public Vehiculo(String placa, String marca, String color, int valorComercial) {
+    public Vehiculo(String placa, String marca, String color, int valorComercial) {//Constructor de 4 parametros
         this.placa = placa;
         this.marca = marca;
         this.color = color;
         this.valorComercial = valorComercial;
 
-        cantidad++;
+        cantidad++;                             //Cada que se crea un vehículo aumenta cantidad en uno
     }
-
+    
+    //Getters & Setters
     public String getPlaca() {
         return placa;
     }
@@ -54,7 +56,9 @@ public class Vehiculo {
     public void setValorComercial(int valorComercial) {
         this.valorComercial = valorComercial;
     }
-
+    
+    
+    //To strings
     @Override
     public String toString() {
         return "Vehiculo{" +
@@ -80,19 +84,41 @@ public class Vehiculo {
         }
         return resultados;
     }
-    public static int cantidadVehiculos() {
+   
+    public static int cantidadVehiculos() { //Indica la cantidad de vehiculos almacenados
         return cantidad;
     }
-
-    public static void ordenarValorComercial(Vehiculo []arreglo){
+    
+    public static String buscarColorDeVehiculo(String color){//Itera sobre la lista de vehiculos para encontrar un vehiculo con el color ingresado
+        String retorno="";
+        int cont=0;
+            for(int i=0;i<Vehiculo.vehiculos.length;i++){
+                for(int j=0;j<Vehiculo.vehiculos[0].length;j++){
+                    if(Vehiculo.vehiculos[i][j]!=null){
+                            if(Vehiculo.vehiculos[i][j].getColor().equalsIgnoreCase(color)){
+                                retorno+="Piso: "+(i+1)+" | Espacio: "+(j+1)+" -> "+Vehiculo.vehiculos[i][j].toString()+'\n';
+                                
+                                cont++;
+                            }
+                        }
+                    }
+                }
+            if(cont==0){
+                    retorno="No se han ingresado vehículos con el color solicitado";
+            }
+            return retorno;
+    }
+    
+    public static void ordenarValorComercial(Vehiculo []arreglo){// Ordena los vehiculos de menor a maayor por valor comercial
         Vehiculo aux;
         for(int i=1;i<arreglo.length;i++){
             for(int j=0;j<arreglo.length-1;j++){
                 if((arreglo[j]!=null)&&(arreglo[j+1]!=null)){
                     if(arreglo[j].getValorComercial()>arreglo[j+1].getValorComercial()){
-                        aux = arreglo[j];
-                        arreglo[j]=arreglo[j+1];
-                        arreglo[j+1]=aux;
+                    aux = arreglo[j];
+                    arreglo[j]=arreglo[j+1];
+                    arreglo[j+1]=aux;
+                    
                     }
                 }
             }
