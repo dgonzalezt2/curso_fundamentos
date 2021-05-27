c97771dba09093b678c9e0e86b5d863fa36d9923
dgonzalezt2 <81880494+dgonzalezt2@users.noreply.github.com>
2021-05-26 22:49:33 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -6,9 +6,10 @@ public class Vehiculo {
     private String marca;
     private String color;
     private int valorComercial;
+    private String tipo;
     public static Vehiculo vehiculos [][];
-    
-    
+
+
     public Vehiculo (){}//Constructor sin parametros
 
     public Vehiculo(String plac, String marc, String colo) {//Constructor con 3 parámetros
@@ -23,7 +24,7 @@ public class Vehiculo {
 
         cantidad++;                             //Cada que se crea un vehículo aumenta cantidad en uno
     }
-    
+
     //Getters & Setters
     public String getPlaca() {
         return placa;
@@ -56,13 +57,22 @@ public class Vehiculo {
     public void setValorComercial(int valorComercial) {
         this.valorComercial = valorComercial;
     }
-    
-    
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
     //To strings
     @Override
     public String toString() {
         return "Vehiculo{" +
                 "placa='" + placa + '\'' +
+                ", tipo='" + tipo +'\'' +
                 ", marca='" + marca + '\'' +
                 ", color='" + color + '\'' +
                 ", valorComercial=" + valorComercial +
@@ -77,48 +87,48 @@ public class Vehiculo {
                 if(vehiculos[row][col]!=null)
                 {
                     Vehiculo carro = vehiculos[row][col];
-                    resultados+= "Vehiculo{" + "placa='" + carro.getPlaca()+ "', marca='" + carro.getMarca() +"', color='" + carro.getColor() +
+                    resultados+= "Vehiculo{" + "placa='" + carro.getPlaca()+ "', tipo='" + carro.getTipo() + "', marca='" + carro.getMarca() +"', color='" + carro.getColor() +
                             "', valorComercial=" + carro.getValorComercial() +'}'+'\n';
                 }
             }
         }
         return resultados;
     }
-   
+
     public static int cantidadVehiculos() { //Indica la cantidad de vehiculos almacenados
         return cantidad;
     }
-    
+
     public static String buscarColorDeVehiculo(String color){//Itera sobre la lista de vehiculos para encontrar un vehiculo con el color ingresado
         String retorno="";
         int cont=0;
-            for(int i=0;i<Vehiculo.vehiculos.length;i++){
-                for(int j=0;j<Vehiculo.vehiculos[0].length;j++){
-                    if(Vehiculo.vehiculos[i][j]!=null){
-                            if(Vehiculo.vehiculos[i][j].getColor().equalsIgnoreCase(color)){
-                                retorno+="Piso: "+(i+1)+" | Espacio: "+(j+1)+" -> "+Vehiculo.vehiculos[i][j].toString()+'\n';
-                                
-                                cont++;
-                            }
-                        }
+        for(int i=0;i<Vehiculo.vehiculos.length;i++){
+            for(int j=0;j<Vehiculo.vehiculos[0].length;j++){
+                if(Vehiculo.vehiculos[i][j]!=null){
+                    if(Vehiculo.vehiculos[i][j].getColor().equalsIgnoreCase(color)){
+                        retorno+="Piso: "+(i+1)+" | Espacio: "+(j+1)+" -> "+Vehiculo.vehiculos[i][j].toString()+'\n';
+
+                        cont++;
                     }
                 }
-            if(cont==0){
-                    retorno="No se han ingresado vehículos con el color solicitado";
             }
-            return retorno;
+        }
+        if(cont==0){
+            retorno="No se han ingresado vehículos con el color solicitado";
+        }
+        return retorno;
     }
-    
+
     public static void ordenarValorComercial(Vehiculo []arreglo){// Ordena los vehiculos de menor a maayor por valor comercial
         Vehiculo aux;
         for(int i=1;i<arreglo.length;i++){
             for(int j=0;j<arreglo.length-1;j++){
                 if((arreglo[j]!=null)&&(arreglo[j+1]!=null)){
                     if(arreglo[j].getValorComercial()>arreglo[j+1].getValorComercial()){
-                    aux = arreglo[j];
-                    arreglo[j]=arreglo[j+1];
-                    arreglo[j+1]=aux;
-                    
+                        aux = arreglo[j];
+                        arreglo[j]=arreglo[j+1];
+                        arreglo[j+1]=aux;
+
                     }
                 }
             }
