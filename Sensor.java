5faddd88d57e353ff9939dd25869a5e396334232
dgonzalezt2 <81880494+dgonzalezt2@users.noreply.github.com>
2021-05-26 22:48:52 -0500

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,13 +1,18 @@
 public class Sensor {
     //Atributos
     public static Sensor[][] sensores;
-    private int estado=0;
+    private int estado = 0;
+    private Date ingreso;
+    private Date salida;
 
     //Constructores
-    public Sensor(){}
+    public Sensor() {
+    }
 
-    public Sensor(int e){
-        this.estado=e;
+    public Sensor(int e) {
+        this.estado = e;
+        Tiempo t1 = new Tiempo();
+        ingreso = t1.getactual();
     }
 
     //Get & Set estado
@@ -17,41 +22,58 @@ public class Sensor {
 
     public void setEstado(int estado) {
         this.estado = estado;
-    }
-
-    @Override
-    public String toString() {
-        if(estado==1){
-            return "Sensor{" + "estado=" + estado + "-Ocupado"+'}';
-        }else{
-            return "Sensor{" + "estado=" + estado + "-Libre"+'}';
+        if (estado == 0) {
+            Tiempo t2 = new Tiempo();
+            salida = t2.getactual();
         }
     }
 
-    public static String sensorLibre(){//Indica los pisos con espacios disponibles
-        String resultados ="";
-        for(int row=0;row<sensores.length;row++){
-            for (int col=0;col<sensores[0].length;col++)
-            {
-                if(sensores[row][col]==null)
-                {
-                    resultados+= "Espacio disponible en : piso["+(row+1)+"] espacio["+(col+1)+"]"+'\n';
+    public Date getIngreso() {
+        return ingreso;
+    }
+
+    public void setIngreso(Date ingreso) {
+        this.ingreso = ingreso;
+    }
+
+    public Date getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Date salida) {
+        this.salida = salida;
+    }
+
+
+    @Override
+    public String toString() {
+        if (estado == 1) {
+            return "Sensor{" + "estado=" + estado + "-Ocupado" + '}';
+        } else {
+            return "Sensor{" + "estado=" + estado + "-Libre" + '}';
+        }
+    }
+
+    public static String sensorLibre() {//Indica los pisos con espacios disponibles
+        String resultados = "";
+        for (int row = 0; row < sensores.length; row++) {
+            for (int col = 0; col < sensores[0].length; col++) {
+                if (sensores[row][col] == null) {
+                    resultados += "Espacio disponible en : piso[" + (row + 1) + "] espacio[" + (col + 1) + "]" + '\n';
                 }
             }
         }
         return resultados;
     }
 
-    public static String sensoresEstado(){//Indica el estado de TODOS los sensores
-        String resultados ="";
-        for(int row=0;row<sensores.length;row++){
-            for (int col=0;col<sensores[0].length;col++)
-            {
-                if(sensores[row][col]==null)
-                {
-                    resultados+= "{piso["+(row+1)+"] espacio["+(col+1)+"]"+ "} estado libre" +'\n';
-                }else{
-                    resultados+= "{piso["+(row+1)+"] espacio["+(col+1)+"]"+ "} estado ocupado" +'\n';
+    public static String sensoresEstado() {//Indica el estado de TODOS los sensores
+        String resultados = "";
+        for (int row = 0; row < sensores.length; row++) {
+            for (int col = 0; col < sensores[0].length; col++) {
+                if (sensores[row][col] == null) {
+                    resultados += "{piso[" + (row + 1) + "] espacio[" + (col + 1) + "]" + "} estado libre" + '\n';
+                } else {
+                    resultados += "{piso[" + (row + 1) + "] espacio[" + (col + 1) + "]" + "} estado ocupado" + '\n';
                 }
             }
         }
